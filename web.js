270f21ce1caa94953990bb3012e00b775f3ee861
Marty Alcala <msalcala11@gmail.com>
2013-07-12 23:14:24 +0000

    The text displayed is now pulled from the index.html file

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fs = require("fs");
+  var buffer = new Buffer();
+  buffer = fs.readFileSync("index.html");
+  var indexStr = buffer.toString("utf-8",0,buffer.length);
+  response.send(indexStr);
 });
 
 var port = process.env.PORT || 5000;
