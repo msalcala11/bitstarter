a68c76f283000ecfb20157ea03b53477423b144b
Marty Alcala <msalcala11@gmail.com>
2013-07-23 16:54:49 +0000

    added functionality to nav bar

---
+++ js/myjs.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+        $('ul.nav > li').click(function (e) {
+            //e.preventDefault();
+            $('ul.nav > li').removeClass('active');
+            $(this).addClass('active');                
+        });            
+    });
+
